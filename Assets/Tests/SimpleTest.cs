3f455c886167a1873468bb3dd3da27bcd59c59c9
Adam Rudas <jotemair@gmail.com>
2019-10-21 15:19:54 +1100

    Added tests

---
+++ Assets/Tests/SimpleTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+public class SimpleTest
+{
+    [Test]
+    public void NUnitTest()
+    {
+        NUnit.Framework.Assert.IsTrue(false);
+    }
+
+    [UnityTest]
+    public IEnumerator UnityTest()
+    {
+        yield return null;
+
+        UnityEngine.Assertions.Assert.IsTrue(false, "Unity Test Failed");
+    }
+}
